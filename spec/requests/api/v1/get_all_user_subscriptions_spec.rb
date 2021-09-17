b419e344b3ca8ac117c4475d3b9dbe4f64cd4c94
Taija <taija.warbelow@gmail.com>
2021-09-17 09:07:00 -0700

    Correct typos, spec passing

--- spec/requests/api/v1/get_all_user_subscriptions_spec.rb
+++ spec/requests/api/v1/get_all_user_subscriptions_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe 'get all user subscriptions' do
     tea1 = create(:tea)
     tea2 = create(:tea)
     subscription = create(:subscription, tea_id: tea.id, customer_id: customer.id, active: true)
-    subscription = create(:subscription, tea_id: tea1.id, customer_id: customer.id, active: true)
-    subscription = create(:subscription, tea_id: tea2.id, customer_id: customer.id, active: false)
-    
-    patch "/api/v1/customers/:id/subscriptions"
+    subscription1 = create(:subscription, tea_id: tea1.id, customer_id: customer.id, active: true)
+    subscription2 = create(:subscription, tea_id: tea2.id, customer_id: customer.id, active: true)
+
+
+
+    get "/api/v1/customers/#{customer.id}/subscriptions"
 
     expect(response.status).to eq(200)
 
@@ -24,14 +26,20 @@ RSpec.describe 'get all user subscriptions' do
     expect(reply[:data][0][:type]).to eq("subscription")
     expect(reply[:data][0][:attributes][:customer_id]).to eq(customer.id)
     expect(reply[:data][0][:attributes][:tea_id]).to eq(tea.id)
-    expect(reply[:data][0][:attributes][:active]).to eq(true)
+    expect(reply[:data][0][:attributes][:active]).to be(subscription.active)
+
+    expect(reply[:data][1][:id]).to eq(subscription1.id)
+    expect(reply[:data][1][:type]).to eq("subscription")
+    expect(reply[:data][1][:attributes][:customer_id]).to eq(customer.id)
+    expect(reply[:data][1][:attributes][:tea_id]).to eq(tea1.id)
+    expect(reply[:data][1][:attributes][:active]).to be(subscription1.active)
 
     expect(reply[:data]).to be_an(Array)
-    expect(reply[:data][2][:id]).to eq(subscription.id)
+    expect(reply[:data][2][:id]).to eq(subscription2.id)
     expect(reply[:data][2][:type]).to eq("subscription")
     expect(reply[:data][2][:attributes][:customer_id]).to eq(customer.id)
-    expect(reply[:data][2][:attributes][:tea_id]).to eq(tea.id)
-    expect(reply[:data][2][:attributes][:active]).to eq(false)
+    expect(reply[:data][2][:attributes][:tea_id]).to eq(tea2.id)
+    expect(reply[:data][2][:attributes][:active]).to eq(subscription2.active)
 
 
   end
