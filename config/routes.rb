a1c6ab13a3d71c8380cd59505733224773eeef75
Taija <taija.warbelow@gmail.com>
2021-09-17 08:49:03 -0700

    Add route to get all a user's subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/customers/:id/subscriptions', to: 'subscriptions#create'
       patch '/subscriptions/:id', to: 'subscriptions#update'
+      get '/customers/:id/subscriptions', to: 'customers/subscriptions#index'
     end
   end
 end
