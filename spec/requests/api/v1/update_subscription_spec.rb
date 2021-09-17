ec20b2c5e5726dc0e71c6db774bdb730b4bfdb05
Taija <taija.warbelow@gmail.com>
2021-09-17 08:38:21 -0700

    Update subscription spec, happy path

--- spec/requests/api/v1/update_subscription_spec.rb
+++ spec/requests/api/v1/update_subscription_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe 'subscription creation' do
   it 'accepts json body and post to create' do
     customer = create(:customer)
     tea = create(:tea)
-    subscription = create(:subscription, tea_id: tea.id, customer_id: customer.id)
+    subscription = create(:subscription, tea_id: tea.id, customer_id: customer.id, active: true)
     json_body = {"active": "false"}
 
-    post "/api/v1/subscriptions/:id", params: json_body
+    patch "/api/v1/subscriptions/#{subscription.id}", params: json_body
 
-    expect(response.status).to eq(201)
+    expect(response.status).to eq(200)
 
     reply = JSON.parse(response.body, symbolize_names: true)
 
-    expect(reply[:data][:id]).to be_an(subscription.id)
+    expect(reply[:data][:id]).to eq(subscription.id)
     expect(reply[:data][:type]).to eq("subscription")
     expect(reply[:data][:attributes][:customer_id]).to eq(customer.id)
     expect(reply[:data][:attributes][:tea_id]).to eq(tea.id)
