01d9aa37db36d330c55f46a582291b591f30aafc
Taija <taija.warbelow@gmail.com>
2021-09-17 14:15:09 -0700

    Rescue not found and bad request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+def record_not_found
+  render status: :not_found
+end
+
+def record_invalid
+  render status: :bad_request
+end
+
 end
