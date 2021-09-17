768a65f495808fbc79597ab832276c679467f544
Taija <taija.warbelow@gmail.com>
2021-09-17 08:38:54 -0700

    Update subscription method, specs passing

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::SubscriptionsController < ApplicationController
     subscription = Subscription.create!(customer_id: params[:id], tea_id: params[:tea_id], active: true)
     render json: SubscriptionSerializer.render(subscription), status: :created
   end
+
+  def update
+    subscription = Subscription.find(params[:id])
+    subscription.update(active: ActiveModel::Type::Boolean.new.cast(params[:active]))
+    render json: SubscriptionSerializer.render(subscription), status: :ok
+  end
 end
