05c4da2a1ee87b49418fb2d2451550a136bac429
Taija <taija.warbelow@gmail.com>
2021-09-17 09:06:43 -0700

    Render all method

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -15,4 +15,22 @@ class SubscriptionSerializer < ActiveModel::Serializer
           }
     }.to_json
   end
+
+  def self.render_all(subscriptions)
+    {
+  data:
+    subscriptions.map do |subscription|
+      {
+        id: subscription.id,
+        type: 'subscription',
+        attributes: {
+          tea_id: subscription.tea_id,
+          customer_id: subscription.customer_id,
+          active: subscription.active,
+        }
+      }
+    end
+}.to_json
+
+  end
 end
