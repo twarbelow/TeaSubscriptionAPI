96bea8ba94d3e04abab15a1810b02f96bdbc4dab
Taija <taija.warbelow@gmail.com>
2021-09-17 21:02:18 -0700

    Logic to check that tea and customer from params exist

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def create
-    subscription = Subscription.create!(customer_id: params[:id], tea_id: params[:tea_id], active: true)
-    render json: SubscriptionSerializer.render(subscription), status: :created
+    if Tea.find(params[:tea_id]) && Customer.find(params[:id])
+      subscription = Subscription.create!(customer_id: params[:id], tea_id: params[:tea_id], active: true)
+      render json: SubscriptionSerializer.render(subscription), status: :created
+    end
   end
 
   def update
