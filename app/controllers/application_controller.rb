3b9c389f68713e6f57fdc6c82e1dc7bbf98a2719
Taija <taija.warbelow@gmail.com>
2021-09-17 21:00:09 -0700

    Remove unnecessary rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::API
-rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-def record_not_found
-  render status: :not_found
-end
-
-def record_invalid
-  render status: :bad_request
-end
-
+  def record_not_found
+    render status: :not_found
+  end
 end
