9131c2a6d8df122c7e6d46e0637a9a97e65f6843
Taija <taija.warbelow@gmail.com>
2021-09-18 13:32:09 -0700

    Add logic for checking validity of active param. Specs passing.

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -7,8 +7,12 @@ class Api::V1::SubscriptionsController < ApplicationController
   end
 
   def update
-    subscription = Subscription.find(params[:id])
-    subscription.update(active: ActiveModel::Type::Boolean.new.cast(params[:active]))
-    render json: SubscriptionSerializer.render(subscription), status: :ok
+    if params[:active] == "true" || params[:active] == "false"
+      subscription = Subscription.find(params[:id])
+      subscription.update(active: ActiveModel::Type::Boolean.new.cast(params[:active]))
+      render json: SubscriptionSerializer.render(subscription), status: :ok
+    else
+      render status: :bad_request
+    end
   end
 end
