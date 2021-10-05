a99090ef55b0f7d819324024c4610b8e1f8315a1
Taija <taija.warbelow@gmail.com>
2021-10-04 22:01:26 -0500

    Customer creation route to customers controller create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/customers', to: 'customers#create'
       post '/customers/:id/subscriptions', to: 'customers/subscriptions#create'
       get '/customers/:id/subscriptions', to: 'customers/subscriptions#index'
 
