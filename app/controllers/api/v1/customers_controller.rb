e0be1de0af86ffc8a80685d32f5eb5d9e33025b7
Taija <taija.warbelow@gmail.com>
2021-10-13 09:24:49 -0700

    Render bad_request if email is already in use, spec passing

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -4,7 +4,9 @@ module Api
   module V1
     class CustomersController < ApplicationController
       def create
-        unless Customer.exists?(email: params[:email])
+        if Customer.exists?(email: params[:email])
+          render status: :bad_request
+        else
           customer = Customer.create!(name: params[:name], email: params[:email])
           render json: CustomerSerializer.render(customer), status: :created
         end
--- spec/requests/api/v1/create_customer_spec.rb
+++ spec/requests/api/v1/create_customer_spec.rb
@@ -20,14 +20,15 @@ RSpec.describe 'customer creation' do
     expect(reply[:data][:attributes][:email]).to eq(email)
   end
 
-  it 'returns 400 if email is already in use'
-  in_use = create(:customer)
-  name = Faker::Name.name
-  email = in_use.email
+  it 'returns 400 if email is already in use' do
+    in_use = create(:customer)
+    name = Faker::Name.name
+    email = in_use.email
 
-  json_body = { "name": name, "email": email }
+    json_body = { "name": name, "email": email }
 
-  post '/api/v1/customers', params: json_body
+    post '/api/v1/customers', params: json_body
 
-  expect(response.status).to eq(400)
+    expect(response.status).to eq(400)
+  end
 end
