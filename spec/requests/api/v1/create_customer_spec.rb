811f069fe47375de9c24ce7b6edf0f7e6af84493
Taija <taija.warbelow@gmail.com>
2021-10-13 09:20:35 -0700

    Email already in use sad path

--- spec/requests/api/v1/create_customer_spec.rb
+++ spec/requests/api/v1/create_customer_spec.rb
@@ -19,4 +19,15 @@ RSpec.describe 'customer creation' do
     expect(reply[:data][:attributes][:name]).to eq(name)
     expect(reply[:data][:attributes][:email]).to eq(email)
   end
+
+  it 'returns 400 if email is already in use'
+  in_use = create(:customer)
+  name = Faker::Name.name
+  email = in_use.email
+
+  json_body = { "name": name, "email": email }
+
+  post '/api/v1/customers', params: json_body
+
+  expect(response.status).to eq(400)
 end
